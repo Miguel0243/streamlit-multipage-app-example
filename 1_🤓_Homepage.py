8fc44460dfcc23d747853e09b32d55b6359ab632
Miguel0243 <127975674+Miguel0243@users.noreply.github.com>
2023-03-29 15:38:43 -0500

    Streamlit Test

--- 1_🤓_Homepage.py
+++ 1_🤓_Homepage.py
@@ -1,12 +1,27 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
-    page_title="Multipage App",
+    page_title="Home",
     page_icon="👋",
 )
 
 st.title("Main Page")
-st.sidebar.success("Select a page above.")
+st.header("This is the header")
+st.subheader("This is the subheader")
+st.text("Hello this is a web page")
+
+st.success("Executed succesfully")
+st.info("This is an information")
+st.warning("This is a warning")
+st.error("An error occured")
+
+st.write("I'm writing this sample code")
+st.write(range(20))
+channel = "Mike123coding"
+st.write("Subscribe to", channel)
+
+st.sidebar.success("Navega a traves del menú superior.")
 
 if "my_input" not in st.session_state:
     st.session_state["my_input"] = ""
@@ -16,3 +31,24 @@ submit = st.button("Submit")
 if submit:
     st.session_state["my_input"] = my_input
     st.write("You have entered: ", my_input)
+
+code_body = '''for i range (10):
+    print (i)'''    
+st.code(code_body,language="python")
+
+if(st.checkbox("Accept")):
+    st.text("You aggreed the condition")
+
+val = st.radio("Select a languaje", ('pthon','java'))
+st.write(val," was selected")
+
+img = Image.open("images/refresh_update_15608.png")
+st.image(img)
+
+option = st.selectbox("Select an option",['python','java','react','node','sql'])
+st.write("You have selected:",option)
+
+options = st.multiselect("Select multiple options",['python','java','node','react','sql'])
+
+sla = st.slider("Your Salary",1000,5000)
+st.write("Your salary is ",sla)
--- pages/2_📚_Projects.py
+++ pages/2_📚_Projects.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import time
+import numpy as np
 
-
+st.set_page_config(page_title="Projects", page_icon="📚")
+st.header("TEST PAGINA WEB PROJECTS")
 st.title("Projects")
-
+st.sidebar.header("Projects")
 st.write("You have entered", st.session_state["my_input"])
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text("%i%% Complete" % i)
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
