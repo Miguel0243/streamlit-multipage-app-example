6d41b0f72dd59b3c64ee26925c9367c97256b7fa
Miguel0243 <127975674+Miguel0243@users.noreply.github.com>
2023-03-30 18:05:00 -0500

    Streamlit bitacora

--- pages/5_📊_Bitacora1.py
+++ pages/5_📊_Bitacora1.py
@@ -1,11 +1,35 @@
 import streamlit as st
 import base64
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Data Frame", page_icon="📊", layout="wide")
 
+page_bg_image = """
+    <style>
+        [data-testid="stAppViewContainer"]{
+            background-color: #F5E2E2;
+            background-size:cover;
+        }
+
+        [data-testid="stHeader"]{
+            background-color: rgba(0,0,0,0);
+        }
+    </style>
+"""
+
+tabs = """
+        <style>
+        [data-baseweb="tab"]{
+            background-color: transparent;
+        }
+"""
+
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True) 
+    st.markdown(page_bg_image,unsafe_allow_html=True)
+    st.markdown(tabs, 
+    unsafe_allow_html=True)
 
 logo_url = "images/reynosa_logo1.png"
 logo = f"url(data:image/png;base64,{base64.b64encode(Path(logo_url).read_bytes()).decode()})"
@@ -29,13 +53,35 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.header("Resumen de Vo. Bo.")
-    st.write("Contenido de la columna 1")
-    
+    st.subheader("75%")
+    with st.container():
+        labels = ["Pendientes","Auorizados"]
+        sizes = [30,70]
+        colors = ['#d3d3d3','#9c1c3c']
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, colors=colors, labels=labels, autopct='%1.1f%%', startangle=90)
+        ax1.axis('equal')
+
+        st.pyplot(fig1)
+
+    with st.container():
+        st.write("---")
+        st.button("Pendiente de Autorizar", use_container_width=True)
+        
 with col2:
     st.header("Proyectos en Proceso")
-    st.write("Contnido de la columna 2")
+    st.subheader("Total: " + str(4))
+    st.button("Nuevo")
+    st.markdown("\n\n---")
+
 
 with col3:
     st.header("Ultimas Notas en Bitacora")
-    st.write("Contenido de la columna 3")
+    st.subheader("Total: " + str(7))
+    mi_lista = ["Elemento 1", "Elemento 2", "Elemento 3", "Elemento 4", "Elemento 5"]
+    for element in mi_lista:
+        st.write("Contratista"+" - "+"Fecha")
+        st.caption("Texto de obra")
 
+tab1,tab2,tab3,tab4,tab5 = st.tabs(['General','Contratista','Supervisor de Obra','Responsable Subsecretaria de Obras Publicas','Responsable Director Supervision de Obras'])
